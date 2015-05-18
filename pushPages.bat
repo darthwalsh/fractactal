a9056026f4ec2572d910e60102b6fcd526c065a1
Carl Walsh <darthwalsh@gmail.com>
2015-05-18 02:39:38 -0700

    fixed pushPages

--- pushPages.bat
+++ pushPages.bat
@@ -1,17 +1,10 @@
 msbuild Fractactal\Fractactal.csproj /t:Rebuild /v:m
-git add -f Fractactal\app.js
-git stash --keep-index
-git reset
-git checkout gh-pages
-rmdir Fractactal /s /q
-git stash pop
-git checkout master Fractactal\index.html
-git checkout master Fractactal\app.css
-git checkout master Fractactal\favicon.ico
-del app.js app.css favicon.ico index.html
-move Fractactal\* .
-rmdir Fractactal
+copy Fractactal\index.html ..\fract-pages\index.html /Y
+copy Fractactal\app.js ..\fract-pages\app.js /Y
+copy Fractactal\app.css ..\fract-pages\app.css /Y
+copy Fractactal\favicon.ico ..\fract-pages\favicon.ico /Y
+pushd ..\fract-pages
 git add --all .
 git commit -m "update gh-pages"
 git push origin gh-pages
-git checkout master
+popd
